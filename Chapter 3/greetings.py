71625b45ef046f68a72681bc51900321e0f5e474
Jake Allen <corningtoptier@gmail.com>
2024-09-28 19:11:14 -0400

    progress: new file added

--- Chapter 3/greetings.py
+++ Chapter 3/greetings.py
@@ -1,5 +1,8 @@
 names = ['Sara', 'Makella', 'Dallas', 'Mike', 'Cindy']
 
+message = " is my friend"
 
+for name in names:
+    print(f'{name}'f'{message}')
 
 
---
+++ Chapter 3/your_own_list.py
@@ -0,0 +1,6 @@
+bikes = ['Honda', 'Harley', 'Kawasaki', 'Suzuski']
+
+message = "I would like to own a "
+
+for bike in bikes:
+    print(f'{message}'f'{bike}')
