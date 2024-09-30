2d13b19e3edd5d833b6d04cbd347f288a75c7d2d
Jake Allen <corningtoptier@gmail.com>
2024-09-30 08:22:10 -0400

    progress: changing guest_list.py

--- Chapter 3/guest_list.py
+++ Chapter 3/guest_list.py
@@ -2,3 +2,18 @@ guests = ['Sara', 'Makella', 'Dallas', 'Mike', 'Cindy']
 
 for guest in guests:
     print(f'{guest}'", there is a dinner party next week. Would you like to come?")
+
+print("")
+
+### ChatGPT describes a different way from the
+## Book and I think I like it more
+
+print("Sara can't make it.")
+index = guests.index('Sara')
+guests[0] = "Karl"
+
+print("")
+
+for guest in guests:
+    print(f'{guest}'", there is a dinner party next week. Would you like to come?")
+
