db2a9e1111a9f58c69b8eaa30351bdfc7e13ca38
Jake Allen <corningtoptier@gmail.com>
2024-09-30 08:44:07 -0400

    file: more_guests.py

---
+++ Chapter 3/shrinking_guest_list.py
@@ -0,0 +1,37 @@
+guests = ['Sara', 'Makella', 'Dallas', 'Mike', 'Cindy']
+
+for guest in guests:
+    print(f'{guest}'", there is a dinner party next week. Would you like to come?")
+
+print("")
+
+### ChatGPT describes a different way from the
+## Book and I think I like it more
+
+print("Sara can't make it.")
+index = guests.index('Sara')
+guests[0] = "Karl"
+
+print("")
+
+for guest in guests:
+    print(f'{guest}'", there is a dinner party next week. Would you like to come?")
+
+print("")
+
+print("WE FOUND A BIGGER TABLE!!")
+
+print("")
+
+guests.insert(0, "Frank")
+guests.insert(3, "Grace")
+guests.append("John")
+
+for guest in guests:
+    print(f'{guest}'", there is a dinner party next week. Would you like to come?")
+
+
+
+
+
+
