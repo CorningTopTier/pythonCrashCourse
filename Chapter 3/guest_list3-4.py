3bed050bc09fdf15c96ae99f6d3d723ee9049c58
Jake Allen <corningtoptier@gmail.com>
2024-10-09 16:24:14 -0400

    progress: finished guest_list3-4.py

--- Chapter 3/guest_list3-4.py
+++ Chapter 3/guest_list3-4.py
@@ -9,3 +9,12 @@ print(guests)
 
 for guest in guests:
     print(f'{guest}'", is still coming")
+
+print("WE FOUND A BIGGER TABLE!!")
+
+guests.insert(0, 'Karl')
+guests.insert(3, 'Toby')
+guests.append("Melody")
+
+for guest in guests:
+    print(f'{guest}'", this is final the finished guest list.")
