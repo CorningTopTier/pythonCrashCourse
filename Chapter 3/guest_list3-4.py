e3d2d7689192bb0efba8ed5efd2d80013621d193
Jake Allen <corningtoptier@gmail.com>
2024-10-09 16:19:38 -0400

    progress: guest_list3-4.py

---
+++ Chapter 3/guest_list3-4.py
@@ -0,0 +1,11 @@
+guests = ['Sara', 'Makella', 'Dallas', 'Mike', 'Cindy']
+print(guests)
+
+print("Sara")
+
+guests.remove('Sara')
+guests.append('Darlene')
+print(guests)
+
+for guest in guests:
+    print(f'{guest}'", is still coming")
