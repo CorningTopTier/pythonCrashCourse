38b3ccb23ae7ede9a5ea64d25f678b172d67785a
Jake Allen <corningtoptier@gmail.com>
2024-10-14 16:59:01 -0400

    update

---
+++ Chapter 4/animals.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'lizard', 'hamster', 'ferret']
+
+for animal in animals:
+    print(f"{animal.title()}"", make great pets!")
+
+print("Any of these animals would make great pets!")
