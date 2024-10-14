eb04bd412f37d2d490f42fccc9128146023290b7
Jake Allen <corningtoptier@gmail.com>
2024-10-14 13:12:06 -0400

    seeing_the_world.py

---
+++ Chapter 3/seeing_the_world.py
@@ -0,0 +1,11 @@
+locations = ['Elmira', 'Corning', 'Big Flats', 'Rochester']
+print(locations)
+print(sorted(locations))
+print(locations)
+locations_reverse = locations.sort(reverse=True)
+print(locations_reverse)
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
