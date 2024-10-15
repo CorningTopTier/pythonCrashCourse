97715c962591f46ab165becb064ec515b9a69d7e
Jake Allen <corningtoptier@gmail.com>
2024-10-15 12:12:45 -0400

    new: summing_a_million.py

---
+++ Chapter 4/summing_a_million.py
@@ -0,0 +1,8 @@
+numbers = list(range(1, 1000001)) # create the list
+
+minimum = min(numbers) # check min value
+maximum = max(numbers) # check max value
+print(f"Minimum: {minimum}, Maximum: {maximum}")
+
+total = sum(numbers) # sum of the numbers
+print(f"Sum: {total}")
