cf3fce45f048c213148d97e9e907f9622045ed89
Jake Allen <corningtoptier@gmail.com>
2024-10-16 13:47:29 -0400

    Try It Yourself: 4-10. Slices

--- Chapter 4/slices.py
+++ Chapter 4/slices.py
@@ -1,1 +1,11 @@
-my_foods = ['pizza', 'falafel', 'carrot cake', 'cannoli']
+my_foods = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'pineapple',
+            'ham', 'corn']
+
+print("The first 3 items are:")
+print(my_foods[:3])
+
+print("\nThree items from the middle of the list are:")
+print(my_foods[3:6])
+
+print("\n The last three items are:")
+print(my_foods[-3:])
