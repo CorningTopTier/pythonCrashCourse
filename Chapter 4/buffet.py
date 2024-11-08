1c934f230b9ad921abf7f9f6defb521b7d1fb545
Jake Allen <corningtoptier@gmail.com>
2024-11-08 10:02:11 -0500

    4-13 - Try It Yourself buffet.py

---
+++ Chapter 4/buffet.py
@@ -0,0 +1,7 @@
+simple_foods = ("corn", "peas", "potatoes", "beans", "eggs")
+for food in simple_foods:
+    print("Our buffet offers " f'{food}'"!")
+print("")
+simple_foods = ("apples", "nuts", "potatoes", "beans", "eggs")
+for food in simple_foods:
+    print("Our buffet offers " f'{food}'"!")
