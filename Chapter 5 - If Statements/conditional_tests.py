7eb5dc625effa72d300df7e09812f7922d5bdcb9
Jake Allen <corningtoptier@gmail.com>
2024-11-09 10:19:07 -0500

    file: conditional_tests.py

---
+++ Chapter 5 - If Statements/conditional_tests.py
@@ -0,0 +1,33 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs car.lower() == 'Subaru'.lower()? I predict True.")
+print(car.lower() == 'Subaru'.lower())
+
+age = 25
+print("\nIs age == 25? I predict True.")
+print(age == 25)
+
+print("\nIs age != 30? I predict True.")
+print(age != 30)
+
+print("\nIs age > 20? I predict True.")
+print(age > 20)
+
+print("\nIs age <= 25? I predict True.")
+print(age <= 25)
+
+colors = ['red', 'green', 'blue']
+print("\nIs 'green' in colors? I predict True.")
+print('green' in colors)
+
+print("\nIs 'yellow' not in colors? I predict True.")
+print('yellow' not in colors)
+
+is_raining = False
+print("\nIs is_raining == False? I predict True.")
+print(is_raining == False)
